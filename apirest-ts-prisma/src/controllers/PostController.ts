591202c7c03ff361c3842d24aea3762cf428a3b8
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-24 21:32:53 -0300

    feat: add PostController controller

---
+++ apirest-ts-prisma/src/controllers/PostController.ts
@@ -0,0 +1,122 @@
+import { Request, Response } from "express";
+import prismaClient from "../prisma";
+
+export default {
+  async createPost(request: Request, response: Response) {
+    try {
+      const { title, content, authorId } = request.body;
+
+      const post = await prismaClient.post.create({
+        data: {
+          title,
+          content,
+          authorId,
+        },
+      });
+
+      return response.json({
+        error: false,
+        message: "Sucesso: Post cadastrado com sucesso!",
+        post,
+      });
+    } catch (error) {
+      return response.json({ message: error.message });
+    }
+  },
+
+  async listPost(request: Request, response: Response) {
+    try {
+      const { id } = request.params;
+
+      const post = await prismaClient.post.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!post) {
+        return response.json({
+          error: true,
+          message: "Error: Post não encontrado!",
+        });
+      }
+
+      return response.json({
+        error: false,
+        post,
+      });
+    } catch (error) {
+      return response.json({ message: error.message });
+    }
+  },
+
+  async updatePost(request: Request, response: Response) {
+    try {
+      const { id, title, content } = request.body;
+
+      const postExists = await prismaClient.post.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!postExists) {
+        return response.json({
+          error: true,
+          message: "Error: Post não encontrado!",
+        });
+      }
+
+      const post = await prismaClient.post.update({
+        where: {
+          id: request.body.id,
+        },
+        data: {
+          title,
+          content,
+        },
+      });
+
+      return response.json({
+        error: false,
+        message: "Sucesso: Post atualizado com sucesso!",
+        post,
+      });
+    } catch (error) {
+      return response.json({ message: error.message });
+    }
+  },
+
+  async deletePost(request: Request, response: Response) {
+    try {
+      const { id } = request.params;
+
+      const postExists = await prismaClient.post.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!postExists) {
+        return response.json({
+          error: true,
+          message: "Error: Post não encontrado!",
+        });
+      }
+
+      const post = await prismaClient.post.delete({
+        where: {
+          id: request.params.id,
+        },
+      });
+
+      return response.json({
+        error: false,
+        message: "Sucesso: Post deletado com sucesso!",
+        post,
+      });
+    } catch (error) {
+      return response.json({ message: error.message });
+    }
+  },
+};
